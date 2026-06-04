4b72d8b54d77a4027d6a5f8c48931317ecbb08ab
gabrielbaye897-bit <gabrielbaye897@gmail.com>
2026-06-04 21:14:22 +0200

    made overall code more coherent (with the introduction of Window_Height and Window_Width to make it more compatible between diferents displays/monitors sizes and remove some hardcoded nonsenses + magic number but not all  )

--- pacman_prod/pacman_prod.py
+++ pacman_prod/pacman_prod.py
@@ -13,7 +13,6 @@ Window_Width = window.winfo_screenwidth()
 screen_resolution = str(Window_Height)+'x'+str(Window_Width)
 window.geometry(screen_resolution)
 
-# select keyboard type # TO-DO / in progress.... / normaly done 
 keyboardtype = None 
 
 def ask_keyborad_layout() : 
@@ -44,72 +43,98 @@ def ask_keyborad_layout() :
 ask_keyborad_layout()
 
 area = tk.Canvas(window ,height = Window_Height, width = Window_Width , bg = "#000000" )
-#area.resizable(True,True)
 area.pack(padx = 5 , pady = 0 )
 
 
 
+pac_x1 ,pac_y1 =100 ,100
+sizeofpac= 100 
+pac_x2 ,pac_y2 = pac_x1 + sizeofpac , pac_y1 + sizeofpac
+AngleStartCircle = 30 
+rateofchange=10
 
-area.create_arc( 100, 100, 200, 200, start = 30, extent = 300 , fill = "yellow" , outline = "yellow" , tag = "pac" )
-# make move 
 
-def Move_Entity_d( a, d, x, y,) :
-    print(a)
-    area.move( d, x, y)
-    area.update()
+def Makepac():
+    area.delete("pac")
+    area.create_arc(
+        pac_x1 ,pac_y1 ,pac_x2 ,pac_y2,
+        start = AngleStartCircle, extent =300 ,
+        fill = "yellow", outline = "black", tag = "pac "
+    )
+
+
+
+
+#this just create it for the first time 
+Makepac()
+
 
 #def the mov for pacman
-def Move_up(event): 
-    Move_Entity_d("up", "pac", 0, 100)
-    area.delete("pac")
-    area.create_arc( 100, 100, 200, 200, start = 120, extent = 300 , fill = "yellow" , outline = "yellow" , tag = "pac" )
+def Move_up(event):
+    global pac_x1 ,pac_y1 ,pac_x2 ,pac_y2 ,AngleStartCircle
+    pac_y1 -= rateofchange
+    pac_y2 -= rateofchange
+    AngleStartCircle = 90
+    Makepac()
+
+    
 
 def Move_down(event): 
-    Move_Entity_d("down", "pac", 0, -100) 
-    area.delete("pac")
-    area.create_arc( 100, 100, 200, 200, start = 300, extent = 300 , fill = "yellow" , outline = "yellow" , tag = "pac" )
+    global pac_x1 ,pac_y1 ,pac_x2 ,pac_y2 ,AngleStartCircle
+    pac_y1 += rateofchange
+    pac_y2 += rateofchange
+    AngleStartCircle = 270
+    Makepac()
+    
 
 def Move_left(event):  
-    Move_Entity_d("left", "pac", 100, 0)
-    area.delete("pac")
-    area.create_arc( 100, 100, 200, 200, start = 210, extent = 300 , fill = "yellow" , outline = "yellow" , tag = "pac" )
+    global pac_x1 ,pac_x2 ,AngleStartCircle
+    pac_x1 -= rateofchange
+    pac_x2 -= rateofchange
+    AngleStartCircle = 180
+    Makepac()
 
 def Move_right(event): 
+    global pac_x1  ,pac_x2  ,AngleStartCircle
+    pac_x1 += rateofchange
+    pac_x2 += rateofchange
+    AngleStartCircle = 30
+    Makepac()
 
-    Move_Entity_d("right", "pac", -100, 0)
-    area.delete("pac")
-    area.create_arc( 100, 100, 200, 200, start = 30, extent = 300 , fill = "yellow" , outline = "yellow" , tag = "pac" )
 
+#I have modify this to be coherent with the overall code 
+# -gab
 
-#def mov for ghost (idk why they didn't fcking use Move_Entity_d() )
-# yeah i just c/p from others_ver/romane/romane_ver.py and this (try) keep ghost in boundaries 
-''' 
+blueghost_x ,blueghost_y = 400,400
+posx_gb = blueghost_x 
+posy_gb = blueghost_y
+
 def Move_ghost():
     global posx_gb , posy_gb
     x = randint(1,4)
 
-    while(posy_gb >= 950):
+    while(posy_gb >= Window_Width):
         x = randint(1,4)
-    while(posx_gb >= 1900):
+    while(posx_gb >= Window_Height):
         x = randint(1,4)
     if (x == 1):
-        zone.move("ghostblue", -100, 0)
-        posx_gb -= 100
-        zone.update()
-        zone.after(1000)
+        area.move("blueghost", -100, 0)
+        posx_gb -= 10
+        area.update()
+        area.after(1000)
     if (x == 2):
-        zone.move("ghostblue", 0, -100)
-        posy_gb -= 100
-        zone.update()
+        area.move("blueghost", 0, -100)
+        posy_gb -= 10
+        area.update()
     if (x == 3):
-        zone.move("ghostblue", 0, 100)
-        posy_gb += 100
-        zone.update()
+        area.move("blueghost", 0, 100)
+        posy_gb += 10
+        area.update()
     if (x == 4):
-        zone.move("ghostblue", 100, 0)
-        posx_gb += 100
-        zone.update()
-'''
+        area.move("blueghost", 100, 0)
+        posx_gb += 10
+        area.update()
+
 #keyboard layout type a/q support (qwerty/azerty)
 if keyboardtype == "a":
     window.bind("<z>", Move_up   )
@@ -136,38 +161,45 @@ window.bind("<Left>",  Move_left )
 window.bind("<Right>", Move_right)
 
 
-#rectangle AB,AC,BD
-area.create_rectangle((100,100), (1800,120), width=4, outline='blue', fill='black')
-area.create_rectangle((100,100),  (120,300), width=4, outline='blue', fill='black')
-area.create_rectangle((1780,100),(1800,500), width=4, outline='blue', fill='black')
+#if someone want to work on this go ahead 
+#nothing keeping you back
+
+
+#this need to be sync with the var :
+#Window_Height
+#Window_Width 
+
+#to be addaptative pls
+#and thanks you  
+'''
+#window_height can be x_top_right_corner
+#rectangle top ,1/2 left 
+#area.create_rectangle((100,100), (1800,120), width=4, outline='blue', fill='black')
+#area.create_rectangle((100,100),  (120,300), width=4, outline='blue', fill='black')
+area.create_rectangle((Window_height,100),(1800,500), width=4, outline='blue', fill='white')
 
 #rectangle EF,GE,FH
-area.create_rectangle((100,830), (1800,850), width=4, outline='blue', fill='black')
-area.create_rectangle((100,330),  (120,850), width=4, outline='blue', fill='black')
-area.create_rectangle((1780,530),(1800,850), width=4, outline='blue', fill='black')
+#area.create_rectangle((100,830), (1800,850), width=4, outline='blue', fill='black')
+#area.create_rectangle((100,330),  (120,850), width=4, outline='blue', fill='black')
+#area.create_rectangle((1780,530),(1800,850), width=4, outline='blue', fill='black')
 
  #petit rectangle:
 #IP, IJ, JK, NO, PO
-area.create_rectangle((720,340), (1080,360), width=4, outline='blue', fill='black')
-area.create_rectangle((720,340),  (740,510), width=4, outline='blue', fill='black')
-area.create_rectangle((720,490),  (885,510), width=4, outline='blue', fill='black')
-area.create_rectangle((915,490), (1080,510), width=4, outline='blue', fill='black')
-area.create_rectangle((1060,340),(1080,510), width=4, outline='blue', fill='black')
+#area.create_rectangle((720,340), (1080,360), width=4, outline='blue', fill='black')
+#area.create_rectangle((720,340),  (740,510), width=4, outline='blue', fill='black')
+#area.create_rectangle((720,490),  (885,510), width=4, outline='blue', fill='black')
+#area.create_rectangle((915,490), (1080,510), width=4, outline='blue', fill='black')
+#area.create_rectangle((1060,340),(1080,510), width=4, outline='blue', fill='black')
  #obstacles
 #QT, UX, Ya, ac
-area.create_rectangle((240,220),(440,420),   width=4, outline='blue', fill='black')
-area.create_rectangle((120,540),(320,560),   width=4, outline='blue', fill='black')
-area.create_rectangle((240,580),(260,730),   width=4, outline='blue', fill='black')#xxx
+#area.create_rectangle((240,220),(440,420),   width=4, outline='blue', fill='black')
+#area.create_rectangle((120,540),(320,560),   width=4, outline='blue', fill='black')
+#area.create_rectangle((240,580),(260,730),   width=4, outline='blue', fill='black')#xxx
+'''
 
 
 
-
-
-
-
-#Move_ghost()
-
-
+Move_ghost()
 
 window.mainloop()
 
